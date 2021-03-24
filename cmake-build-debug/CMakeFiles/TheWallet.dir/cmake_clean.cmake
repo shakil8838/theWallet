4bfdc0a0371b880289e790618391f807202a93ae
shakil8838 <eng.shakil8838@gmail.com>
2021-03-24 19:01:29 +0600

    init git

---
+++ cmake-build-debug/CMakeFiles/TheWallet.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TheWallet.dir/main.c.obj"
+  "TheWallet.exe"
+  "TheWallet.exe.manifest"
+  "TheWallet.pdb"
+  "libTheWallet.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TheWallet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void showMenu();
+void getInput();
+void showWallet();
+void spend();
+void insertData(int start, int limit);
+
+int walletAry[100];
+int arySize = 0;
+
+bool isAryDefined = false;
+
+int main() {
+
+    printf("Hi, what would you like to do? press the digit to operate");
+    showMenu();
+
+    return 0;
+}
+
+// Saif please commit your code here
+void showMenu() {}
+
+// Riaz please commit your code here
+void getInput() {}
+
+// Kabir please commit your code here
+void insertData(int start, int limit) {}
+
+// Shakil commit here
+void showWallet() {}
+
+// Parvez please commit your code here
+void spend() {}
